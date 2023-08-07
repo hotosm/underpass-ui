fa3f5ff2a93df6281fd9e2a7be538d63a21e02a1
Emillio Mariscal <emi420@gmail.com>
2023-08-07 10:18:17 -0300

    Initial commit: Underpass UI

---
+++ src/fixtures/UnderpassMapNodes.fixture.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import UnderpassMapNodes from '../components/Underpass/UnderpassMapNodes';
+import { center } from './center';
+
+export default <UnderpassMapNodes 
+  center={center}
+  tagKey="amenity"
+  tagValue=""
+/>;
---
+++ src/fixtures/center.js
@@ -0,0 +1,2 @@
+
+export const center=[-30.9586147, -64.2734073].reverse()
