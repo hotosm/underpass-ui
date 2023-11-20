baed06820c2d585a785b537793c16233f0547cbf
Emillio Mariscal <emi420@gmail.com>
2023-11-20 17:01:40 -0300

    + Infinite scroll for pagination

--- src/components/UnderpassFeatureList/index.jsx
+++ src/components/UnderpassFeatureList/index.jsx
@@ -21,7 +21,9 @@ function UnderpassFeatureList({
 
   const [features, setFeatures] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(false);
   const realtimeIntervalRef = useRef();
+  const infiniteScrollRef = useRef(null);
 
   async function fetch(page) {
     if (!loading) {
@@ -40,8 +42,13 @@ function UnderpassFeatureList({
           } else {
             setFeatures(data);
           }
+          if (data && data.length > 0) {
+            setHasMore(true);
+            onUpdate && onUpdate(data[0]);
+          } else {
+            setHasMore(false);
+          }
           setLoading(false);
-          onUpdate && onUpdate(data[0]);
         },
         onError: (error) => {
           console.log(error);
@@ -52,6 +59,7 @@ function UnderpassFeatureList({
 
   useEffect(() => {
     fetch();
+    infiniteScrollRef.current.pageLoaded = 0;
   }, [tags, hashtag, status, area]);
 
   useEffect(() => {
@@ -70,20 +78,21 @@ function UnderpassFeatureList({
       <InfiniteScroll
         pageStart={0}
         loadMore={fetch}
-        hasMore={false}
-        loader={<div className="loader" key={0}>Loading ...</div>}
+        hasMore={hasMore}
         useWindow={false}
+        ref={infiniteScrollRef}
       >
         {features && features.map(feature => (
-          <div key={feature.id} onClick={() => {
+          feature && (
+          <div key={feature && feature.id} onClick={() => {
             onSelect && onSelect(feature)
-          }}>
+          }}> 
             <FeatureDetailCard
               key={feature.id}
               feature={feature}
             />
           </div>
-        ))}
+        )))}
       </InfiniteScroll>
       {!loading && features && features.length == 0 &&
         <span className={styles.noResults}>No results</span>
